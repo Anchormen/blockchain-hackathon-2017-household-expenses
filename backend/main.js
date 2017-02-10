5b49059a20a6812539dc8e26c242318e5e8267e5
Jeroen Vlek <j.vlek@anchormen.nl>
2017-02-10 21:17:50 +0100

    Added express hello world

--- backend/main.js
+++ backend/main.js
@@ -4,7 +4,7 @@ const loginHandler = require('./login.handler')
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => loginHandler(req, res));
+const server = http.createServer((req, res) => loginHandler(req, res);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
