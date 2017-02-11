a88ef4896d0dedba79976fcac9f4229a157d92af
l.stout <l.stout@anchormen.nl>
2017-02-11 22:39:41 +0100

    add test to get payment correctly

--- backend/test/run_tests.spec.js
+++ backend/test/run_tests.spec.js
@@ -1,13 +1,27 @@
 
 var mainRef = require("../server.js");
 var TestContract = require('../blockchainwrapper/contract.BalanceBook.handler.js');
+var contractsHandler = require('../blockchainwrapper/contracts.handler.js');
+web3 = contractsHandler.web3
 
 describe("BlockChainWrapperTests", function(){
     describe("TestContractUserBalanceIs20", function() {
-        it("returns balance of 20", function(done) {
-            bal = TestContract.getBalance();
-            expect(bal).toBe(20);
-            done()
+    	household = 23;
+    	coach_address = web3.eth.accounts[0];
+    	n = 1;
+    	g = 2;
+    	company = 'FU';
+    	encrypted_change = 20;
+        it("adds a payment correctly.", function(done) {
+        	web3.personal.unlockAccount(coach_address, 'MYPASSPHRASE');
+            TestContract.addHouseHold(household, n, g, coach_address);
+            TestContract.addPayment(household, company, encrypted_change, coach_address);
+            setTimeout(()=>{
+            	payment = TestContract.getPayment(household, company);
+            	expect(payment['amount']).toBe(encrypted_change);
+            	expect(payment['active']).toBe(true);
+            	done();
+            }, 1000);
         });
     });
 });
