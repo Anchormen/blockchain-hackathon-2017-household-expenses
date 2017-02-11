ca22c8fe41b717f685ccc168fcfac2003b8fd44e
lqrz <lautaro.quiroz@gmail.com>
2017-02-11 15:51:30 +0100

    jwt token creation

--- backend/user/login.handler.js
+++ backend/user/login.handler.js
@@ -1,4 +1,5 @@
-
+var jwtSecretKey = require('../config/config');
+var jwtHandler = require('jsonwebtoken');
 
 let processLogin = function (req, res) {
   if(req.body.username === 'anchormen' && req.body.password === 'anchormen') {
@@ -11,11 +12,17 @@ let processLogin = function (req, res) {
 
 function loginOk(req, res) {
   console.log("Login succesful")
-  res.status(200).json({ "login": true})
+  var jwtToken = createJwtToken(req.body.username);
+  res.status(200).json({ "login": true, "jwtToken": jwtToken})
 }
 
 function loginFailed(req, res) {
   res.status(401).json({ "login": false})
+}
+
+function createJwtToken(username){
+	var token = jwtHandler.sign({ data: username }, jwtSecretKey.secret, { expiresIn: 60 * 1 });
+	return token;
 }
 
 module.exports = processLogin
