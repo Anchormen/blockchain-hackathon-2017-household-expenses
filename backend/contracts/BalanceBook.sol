2ba5ade81366416008b5b5cc4b33060d341bb0c8
l.stout <l.stout@anchormen.nl>
2017-02-11 17:45:59 +0100

    Finished balance book

--- backend/contracts/BalanceBook.sol
+++ backend/contracts/BalanceBook.sol
@@ -1,8 +1,15 @@
 pragma solidity ^0.4.4;
 
 contract BalanceBook{
-	mapping (uint => address) private coaches;
-	mapping (uint => int) private balances;
+	struct Payment{
+		int encrypted_change;
+		bool active;
+	}
+
+	mapping (uint => address) coaches;
+	mapping (uint => mapping (string => Payment)) private payments;
+	mapping (uint => uint) public public_ns;
+	mapping (uint => uint) public public_gs;
 	address public owner;
 
 	function BalanceBook() {
@@ -15,21 +22,86 @@ contract BalanceBook{
 		}
 	}
 
-	function addHouseHold(uint household) {
+	function addHouseHold(uint household, uint n, uint g) {
 		coaches[household] = msg.sender;
-		balances[household] = 0;
+		public_ns[household] = n * n;
+		public_gs[household] = g;
 	}
 
 	function removeHouseHold(uint household) onlyCoach(household) {
 		delete coaches[household];
-		delete balances[household];
+		delete public_ns[household];
+		delete public_gs[household];
 	}
 
-	function modifyBalance(uint household, int amount) onlyCoach(household) {
-		balances[household] += amount;
+	function addPayment(uint household, string company, int encrypted_change, bool active) onlyCoach(household) {
+		Payment memory payment = Payment(encrypted_change, active);
+		payments[household][company] = payment;
 	}
 
-	function getBalance(uint household) onlyCoach(household) returns(int) {
-		return balances[household];
+	function togglePayment(uint household, string company) onlyCoach(household) {
+		payments[household][company].active = !payments[household][company].active;
+	}
+
+	function getPayment(uint household, string company) returns (int, bool) {
+	    Payment payment = payments[household][company];
+	    return (payment.encrypted_change, payment.active);
+	} 
+
+	function getPublicKeys(uint household) returns (uint, uint) {
+		return (public_ns[household], public_gs[household]);
+	}
+}pragma solidity ^0.4.4;
+
+contract BalanceBook{
+	struct Payment{
+		int encrypted_change;
+		bool active;
+	}
+
+	mapping (uint => address) coaches;
+	mapping (uint => mapping (string => Payment)) private payments;
+	mapping (uint => uint) public public_ns;
+	mapping (uint => uint) public public_gs;
+	address public owner;
+
+	function BalanceBook() {
+	    owner = msg.sender;
+	}
+
+	modifier onlyCoach(uint household) {
+		if (msg.sender == coaches[household]) {
+			_;
+		}
+	}
+
+	function addHouseHold(uint household, uint n, uint g) {
+		coaches[household] = msg.sender;
+		public_ns[household] = n * n;
+		public_gs[household] = g;
+	}
+
+	function removeHouseHold(uint household) onlyCoach(household) {
+		delete coaches[household];
+		delete public_ns[household];
+		delete public_gs[household];
+	}
+
+	function addPayment(uint household, string company, int encrypted_change, bool active) onlyCoach(household) {
+		Payment memory payment = Payment(encrypted_change, active);
+		payments[household][company] = payment;
+	}
+
+	function togglePayment(uint household, string company) onlyCoach(household) {
+		payments[household][company].active = !payments[household][company].active;
+	}
+
+	function getPayment(uint household, string company) returns (int, bool) {
+	    Payment payment = payments[household][company];
+	    return (payment.encrypted_change, payment.active);
+	} 
+
+	function getPublicKeys(uint household) returns (uint, uint) {
+		return (public_ns[household], public_gs[household]);
 	}
 }
